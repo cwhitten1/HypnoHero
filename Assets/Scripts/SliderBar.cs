7177b90223dec2f79b46e705f29d9ec73c480fd4
karnany26 <karnany26@gmail.com>
2016-03-15 20:18:57 -0400

    Added the slider and text

---
+++ Assets/Scripts/SliderBar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SliderBar : MonoBehaviour {
+
+	// Use this for initialization
+	
+	
+	// Update is called once per frame
+	void Update () {
+        Slider slider = GetComponent<Slider>();
+        slider.value = Mathf.MoveTowards(slider.value, 1f, Time.deltaTime * 0.5f);
+
+    }
+}
