ae8e54fdaf718616d13602d15a4cb77890b9115c
Phillip <smith.phillip.m@live.com>
2016-03-23 10:05:02 -0400

    Added Slime

---
+++ Assets/Scripts/Player/EnemyHealth.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+internal class EnemyHealth
+{
+    internal void TakeDamage(int damagePerShot, Vector3 point)
+    {
+        throw new NotImplementedException();
+    }
+}
