21610e8808bf7e2539042bac867aee959f904d1f
karnany26 <karnany26@gmail.com>
2016-03-24 12:54:09 -0400

    Added increase and decrease functions for Slider Bars

--- Assets/Scripts/SliderBar.cs
+++ Assets/Scripts/SliderBar.cs
@@ -4,13 +4,34 @@ using System.Collections;
 
 public class SliderBar : MonoBehaviour {
 
-	// Use this for initialization
-	
-	
-	// Update is called once per frame
-	void Update () {
-        Slider slider = GetComponent<Slider>();
-        slider.value = Mathf.MoveTowards(slider.value, 1f, Time.deltaTime * 0.5f);
+    void increase_val (string name, float val) {
+        Slider slider = GameObject.Find(name).GetComponent<Slider>();
+        slider.value += val;
+      //  slider.value = Mathf.MoveTowards(slider.value, val, Time.deltaTime * 0.5f);
 
+    }
+
+    void decrease_val(string name, float val)
+    {
+        Slider slider = GameObject.Find(name).GetComponent<Slider>();
+        if (slider.value>0)
+            slider.value -= val;
+        
+       // slider.value = Mathf.MoveTowards(slider.value, val, Time.deltaTime * (-0.5f));
+    }
+
+    void Start()
+    {
+        Slider slider_M = GameObject.Find("ScareMeter").GetComponent<Slider>();
+        Slider slider_C = GameObject.Find("ConfidenceMeter").GetComponent<Slider>();
+        slider_M.value = 0;
+        slider_C.value = 0;
+        increase_val("ScareMeter", 0.4f);
+        decrease_val("ScareMeter", 0.3f);
+
+        increase_val("ConfidenceMeter", 0.4f);
+        decrease_val("ConfidenceMeter", 0.3f);
+        increase_val("ConfidenceMeter", 0.2f);
+    
     }
 }
