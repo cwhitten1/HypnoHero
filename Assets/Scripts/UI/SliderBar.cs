5ee03c9222df8eb43c87d0676d90550ee659c27c
karnany26 <karnany26@gmail.com>
2016-03-25 15:27:00 -0400

    Color change for Sliders

---
+++ Assets/Scripts/UI/SliderBar.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SliderBar : MonoBehaviour {
+
+    void increase_val (string name, float val) {
+        Slider slider = GameObject.Find(name).GetComponent<Slider>();
+        slider.value += val;
+      //  slider.value = Mathf.MoveTowards(slider.value, val, Time.deltaTime * 0.5f);
+
+    }
+
+    void decrease_val(string name, float val)
+    {
+        Slider slider = GameObject.Find(name).GetComponent<Slider>();
+        if (slider.value>0)
+            slider.value -= val;
+        
+       // slider.value = Mathf.MoveTowards(slider.value, val, Time.deltaTime * (-0.5f));
+    }
+
+    void Start()
+    {
+        Slider slider_M = GameObject.Find("ScareMeter").GetComponent<Slider>();
+        Slider slider_C = GameObject.Find("ConfidenceMeter").GetComponent<Slider>();
+        slider_M.value = 0;
+        slider_C.value = 0;
+        increase_val("ScareMeter", 0.4f);
+       
+
+        increase_val("ConfidenceMeter", 0.4f);
+        decrease_val("ConfidenceMeter", 0.3f);
+        increase_val("ConfidenceMeter", 0.2f);
+    
+    }
+}
---
+++ Assets/Scripts/UI/textUpdate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class textUpdate : MonoBehaviour
+{
+    Text txt;
+    // Use this for initialization
+  
+    // Update is called once per frame
+    void Update()
+    {
+        txt = gameObject.GetComponent<Text>();
+        Slider slider_M = GameObject.Find("ScareMeter").GetComponent<Slider>();
+        Slider slider_C = GameObject.Find("ConfidenceMeter").GetComponent<Slider>();
+        double scare_meter = System.Math.Round(slider_M.value, 1) * 100;
+        double confidence_meter = System.Math.Round(slider_C.value, 1) * 100;
+        txt.text = "Scare Meter: " + scare_meter+"%" + "\n\n Confidence Meter: " + confidence_meter+"%"; 
+    }
+}
