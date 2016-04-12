3f81a71849310ba52c535c78eb268f42b6c0b5e4
Matt Lemmone <mattlemmone@att.net>
2016-04-12 02:12:21 -0400

    Added player color toggle for stealth. Took FOREVER!

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,42 +3,53 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public float speed = 6f;
+    public float speed = 6f;
     public float stealthDistance = 5f;
-    public bool isStealth = false;
-	private float rotationSmoothing = 10f;
-    
 
-	Vector3 movement;
-	Animation anim;
-	Rigidbody playerRigidBody;
-    Transform player;               // Reference to the player's position.
+    public Color normColor = new Color(64f, 64f, 64f, 255f);
+    public Color stealthColor = new Color(208f, 208f, 208f, 255f);
+
+    public bool isStealth = false;
+    private float rotationSmoothing = 10f;
+
+
+    Vector3 movement;
+    Animation anim;
+
+    Rigidbody playerRigidBody;
+    Transform player;
+    SkinnedMeshRenderer playerModel;
+
     GameObject[] stealthObjects;
 
     int floorMask;
-	float camRayLength = 100f;
+    float camRayLength = 100f;
 
-	void Awake(){
-		floorMask = LayerMask.GetMask ("Floor");
-		anim = GetComponent<Animation> ();
-		playerRigidBody = GetComponent<Rigidbody> ();
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animation>();
+        playerRigidBody = GetComponent<Rigidbody>();
 
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerModel = GameObject.FindGameObjectWithTag("PlayerModel").GetComponent<SkinnedMeshRenderer>();
         stealthObjects = GameObject.FindGameObjectsWithTag("Stealth");
     }
 
-	void FixedUpdate(){
-		float h = Input.GetAxisRaw("Horizontal"); //Raw axis returns either -1,0,1
-		float v = Input.GetAxisRaw("Vertical");
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal"); //Raw axis returns either -1,0,1
+        float v = Input.GetAxisRaw("Vertical");
 
 
-        Move (h, v);
-		DiscreteTurning (h, v);
-		//Turning ();
-		//Animating (h, v);
-	}
+        Move(h, v);
+        DiscreteTurning(h, v);
+        //Turning ();
+        //Animating (h, v);
+    }
 
-	void Update(){
+    void Update()
+    {
         foreach (var obj in stealthObjects)
         {
             float objX = obj.transform.position.x,
@@ -50,76 +61,94 @@ public class PlayerMovement : MonoBehaviour
             if (distanceFromPlayer <= stealthDistance)
             {
                 Debug.Log("Player is stealth!");
+
+                //Change Colors
                 isStealth = true;
+                SetStealth(isStealth);
                 return;
             }
-            else {
-                isStealth = false;
+            else
+            {
                 Debug.Log("Player isn't stealth!");
+                isStealth = false;
+                SetStealth(isStealth);
             }
         }
 
-		float h = Input.GetAxisRaw("Horizontal"); //Raw axis returns either -1,0,1
-		float v = Input.GetAxisRaw("Vertical"); 
+        float h = Input.GetAxisRaw("Horizontal"); //Raw axis returns either -1,0,1
+        float v = Input.GetAxisRaw("Vertical");
 
-		if (h != 0 || v != 0)
-			anim.CrossFade ("Walk");
-		else
-			anim.CrossFade ("Wait");
+        if (h != 0 || v != 0)
+            anim.CrossFade("Walk");
+        else
+            anim.CrossFade("Wait");
 
-	}
+    }
+    void SetStealth(bool stealth)
+    {
+        playerModel.sharedMaterials[0].SetColor("_Color", stealth ? stealthColor : normColor);
+        playerModel.sharedMaterials[1].SetColor("_Color", stealth ? stealthColor : normColor);
 
-	void Move(float h, float v){
-		movement.Set (h, 0, v);
-		movement = movement.normalized * speed * Time.deltaTime;
-		playerRigidBody.MovePosition (transform.position + movement);
-	}
+        isStealth = stealth;
+    }
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidBody.MovePosition(transform.position + movement);
+    }
 
-	void Turning(){
-		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-		RaycastHit floorHit;
+        RaycastHit floorHit;
 
-		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) {
-			Vector3 playerToMouse = floorHit.point - transform.position;
-			playerToMouse.y = 0f;
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
 
-			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
-			playerRigidBody.MoveRotation (newRotation);
-		}
-	}
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidBody.MoveRotation(newRotation);
+        }
+    }
 
-	void DiscreteTurning(float h, float v){
-		float rotationY = 0;
+    void DiscreteTurning(float h, float v)
+    {
+        float rotationY = 0;
 
-		//Sorry for the conditional logic block
-		if (v == 1) {
-			if (h == 0)
-				rotationY = 0;
-			else if (h == 1)
-				rotationY = 45;
-			else if (h == -1)
-				rotationY = -45;
-		} 
-		else if (v == -1) {
-			
-			if (h == 0)
-				rotationY = 180;
-			else if (h == 1)
-				rotationY = 135;
-			else if (h == -1)
-				rotationY = -135;
-		} 
-		else if (v == 0){
-			if (h == 0)
-				return;
-			else if (h == 1)
-				rotationY = 90;
-			else if (h == -1)
-				rotationY = -90;
-		}
-			
-		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler (0, rotationY, 0), Time.deltaTime * rotationSmoothing);
-	}
-		
+        //Sorry for the conditional logic block
+        if (v == 1)
+        {
+            if (h == 0)
+                rotationY = 0;
+            else if (h == 1)
+                rotationY = 45;
+            else if (h == -1)
+                rotationY = -45;
+        }
+        else if (v == -1)
+        {
+
+            if (h == 0)
+                rotationY = 180;
+            else if (h == 1)
+                rotationY = 135;
+            else if (h == -1)
+                rotationY = -135;
+        }
+        else if (v == 0)
+        {
+            if (h == 0)
+                return;
+            else if (h == 1)
+                rotationY = 90;
+            else if (h == -1)
+                rotationY = -90;
+        }
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, rotationY, 0), Time.deltaTime * rotationSmoothing);
+    }
+
 }
